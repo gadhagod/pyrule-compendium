723ca7e1e96d514dea935b1d6b0b9b2af0af4a28
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-04-03 23:38:50 -0700

    Get version of release for 'publish-package' action, test all src files in mypy test

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 import setuptools
+from os import getenv
 
 setuptools.setup(
-    name='pyrule-compendium',
-    version='2.3.0',
-    author='Aarav Borthakur',
-    author_email='gadhaguy13@gmail.com',
-    description='The official python wrapper for the Hyrule Compendium API',
-    long_description=open('README.md', 'r').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/gadhagod/pyrule-compendium',
+    name="pyrule-compendium",
+    version=getenv("RELEASE_NUM"),
+    author="Aarav Borthakur",
+    author_email="gadhaguy13@gmail.com",
+    description="The official python wrapper for the Hyrule Compendium API",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gadhagod/pyrule-compendium",
     packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    install_requires=['requests']
+    python_requires=">=3.6",
+    install_requires=["requests"]
 )
