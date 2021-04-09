34a3833e438ef7c114d6f8b07caae62017d6bf02
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-04-08 22:35:33 -0700

    Add type hints for utils.py

--- pyrule_compendium/utils.py
+++ pyrule_compendium/utils.py
@@ -1,7 +1,7 @@
-from typing import Union
+from typing import Union, Any
 from requests import get
 
-def api_req(url: str, timeout: types.timeout): return get(url, timeout=timeout).json()["data"]
+def api_req(url: str, timeout: types.timeout) -> Union[list, dict]: return get(url, timeout=timeout).json()["data"]
 
 class types:
     entry = Union[int, str]
@@ -9,5 +9,5 @@ class types:
 
 class api():
     def __init__(self, base_url: str): self.base_url=base_url
-    def endpoint(self, target_endpoint: str): return f"{self.base_url}{target_endpoint}"
-    def request(self, endpoint: str, timeout: types.timeout): return api_req(f"{self.endpoint(endpoint)}", timeout)
+    def endpoint(self, target_endpoint: str) -> str: return f"{self.base_url}{target_endpoint}"
+    def request(self, endpoint: str, timeout: types.timeout) -> Any: return api_req(f"{self.endpoint(endpoint)}", timeout)
