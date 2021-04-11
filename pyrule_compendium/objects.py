a48a96767f7bbb30c7c8c1c3eab3858ee2d340a0
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-04-11 11:05:26 -0700

    Add comments to objects.py

--- pyrule_compendium/objects.py
+++ pyrule_compendium/objects.py
@@ -3,6 +3,25 @@ from shutil import copyfileobj
 from .utils import *
 
 class entry_image(object):
+    """
+    Entry image object.
+
+    Parameters:
+        * `entry_data`: The data on the target entry, usually retrieved with `compendium.get_entry`
+            - type: dict
+        * `_api`: Instance of `utils.api`
+            - type: `utils.api`
+
+    Attributes:
+        * `_api`: `_api` parameter.
+        * `entry_data`: `entry_data` parameter.
+        * `url`: The URL of the image.
+            - type: str
+        * `res`: Response object of request.
+            - type: `requests.models.Response`
+        * `binary`: Raw binary image.
+            - type: str
+    """
     def __init__(self, entry_data: dict, _api: api):
         self._api = api
         self.entry_data = entry_data
@@ -11,6 +30,14 @@ class entry_image(object):
         self.binary = self.res.text
 
     def download(self, output_file=None) -> None: 
+        """
+        Downloads the entry image object.
+
+        Parameters:
+            * `output_file`: The file to write the image to.
+                type: str
+                default: `None`, meaning the entry name with spaces replaced with underscores with a ".png" extension.
+        """
         if not output_file:
             entry_name = self.entry_data["name"].replace(' ', '_')
             output_file = f"{entry_name}.png"
