752e47a827ed76639d82669136736f74426be30c
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-04-10 19:54:02 -0700

    Fix type hints

--- pyrule_compendium/objects.py
+++ pyrule_compendium/objects.py
@@ -3,10 +3,10 @@ from shutil import copyfileobj
 from .utils import *
 
 class entry_image(object):
-    def __init__(self, entry_data: types.entry, _api: api):
+    def __init__(self, entry_data: dict, _api: api):
         self._api = api
         self.entry_data = entry_data
-        self.url = _api.endpoint(f"/entry/{entry_data['name']}/image")
+        self.url: str = _api.endpoint(f"/entry/{entry_data['name']}/image")
         self.res: Response = get(self.url, stream=True)
         self.binary = self.res.text
 
